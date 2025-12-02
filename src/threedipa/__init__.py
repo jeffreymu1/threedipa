90f1009183adda0b2fa2287da4d53f1466d8420a
Jovan-Kemp <jtk194@gmail.com>
2025-12-02 16:33:45 -0500

    Fixes for lab buidling

--- src/threedipa/__init__.py
+++ src/threedipa/__init__.py
@@ -1,35 +1,10 @@
 """
-vizlab3d: A package for 3D vision psychology experiments using PsychoPy
+threedipa: A package for 3D vision psychology experiments using PsychoPy
 
-This package provides tools for creating stereoscopic vision experiments
-using haploscope displays and PsychoPy.
-
-Usage:
-    Import submodules to access functionality:
-    
-    >>> from threedipa.renderer import HaplscopeRender2D
-    >>> from threedipa.stimuli import Stimulus2DImage
-    >>> import threedipa.utils as utils
 """
 
 __version__ = "0.0.1"
 
-# Expose submodules for direct import
-from . import renderer
-from . import stimuli
-from . import utils
-from . import procedure
-from . import monitor_calibration
-from . import config
-from . import initVariables
-
 __all__ = [
     "__version__",
-    "renderer",
-    "stimuli",
-    "utils",
-    "procedure",
-    "monitor_calibration",
-    "config",
-    "initVariables",
 ]
